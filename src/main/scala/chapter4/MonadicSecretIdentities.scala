b914fdd49d13fa93d34757b1d62cd342231496a8
Pavlo Mitin <mitin.pavel@gmail.com>
2018-03-10 16:25:34 +0000

    4.3.1 Use cats.ID instead of home-made Id type

--- src/main/scala/chapter4/MonadicSecretIdentities.scala
+++ src/main/scala/chapter4/MonadicSecretIdentities.scala
@@ -1,11 +1,10 @@
 package chapter4
 
+import cats.Id
+
 object MonadicSecretIdentities extends App {
-  type Id[A] = A
 
-  def Id[A](a: A): Id[A] = a
-
-  def pure[A](a: A): Id[A] = Id[A](a)
+  def pure[A](a: A): Id[A] = a
 
   def map[A, B](a: Id[A])(func: A => B): Id[B] = func(a)
 
@@ -13,5 +12,5 @@ object MonadicSecretIdentities extends App {
 
   println(pure(1))
   println(map(pure(1)) { a => (a + 1).toString })
-  println(flatMap(pure(1)) { a => Id((a + 1).toString) })
+  println(flatMap(pure(1)) { a => (a + 1).toString })
 }
