bf9814a978d9ec103bee9a2665dd727d4f01d231
Pavlo Mitin <mitin.pavel@gmail.com>
2018-03-14 17:48:45 +0000

    4.6.5 Refactor according to the book solution

--- src/main/scala/chapter4/SaferFoldingUsingEval.scala
+++ src/main/scala/chapter4/SaferFoldingUsingEval.scala
@@ -11,20 +11,21 @@ object Folder {
         acc
     }
 
-  def foldRight[A, B](as: List[A], acc: B)(fn: (A, B) => B): B = {
-    foldEvalRight(as, acc)(fn).value
-  }
+  def foldRight[A, B](as: List[A], acc: B)(fn: (A, B) => B): B =
+    foldEvalRight(as, Eval.now(acc)) { (a, b) =>
+      b.map(fn(a, _))
+    }.value
 
-  private def foldEvalRight[A, B](as: List[A], acc: B)(fn: (A, B) => B): Eval[B] =
+  private def foldEvalRight[A, B](as: List[A], acc: Eval[B])(fn: (A, Eval[B]) => Eval[B]): Eval[B] =
     as match {
       case head :: tail =>
-        Eval.defer(foldEvalRight(tail, fn(head, acc))(fn))
+        Eval.defer(fn(head, foldEvalRight(tail, acc)(fn)))
       case Nil =>
-        Eval.now(acc)
+        acc
     }
 }
 
 object SaferFoldingUsingEval extends App {
-  val folded = Folder.recursiveFoldRight((1 to 50000).toList, 0)(_ + _)
+  val folded = Folder.foldRight((1 to 50000).toList, 0)(_ + _)
   println(folded)
 }
