03ede84708510077c6e78aa421f72054a41e7688
Pavlo Mitin <mitin.pavel@gmail.com>
2018-03-27 18:26:24 +0100

    6.4.4 Exercise: Form Validation
    "getValue" and "parseInt" have been refactored according to the book solution

--- src/main/scala/chapter6/FormValidation.scala
+++ src/main/scala/chapter6/FormValidation.scala
@@ -1,21 +1,21 @@
 package chapter6
 
 import cats.syntax.either._
+import cats.data.Validated
 
 case class User(name: String, age: Int)
 
 object FormValidation extends App {
 
-  type FailEither[A] = Either[List[String], A]
+  type FormData = Map[String, String]
+  type FailFast[A] = Either[List[String], A]
+  type FailSlow[A] = Validated[List[String], A]
 
-  def getValue(fieldName: String)(data: Map[String, String]): FailEither[String] =
-    data.get(fieldName) match {
-      case Some(value) => Right(value)
-      case None => Left(List(s"Unexpected field $fieldName"))
-    }
+  def getValue(name: String)(data: FormData): FailFast[String] =
+    data.get(name).
+      toRight(List(s"$name field not specified"))
 
-  def parseInt(string: String): FailEither[Int] =
-    scala.util.Try(string.toInt).
-      toEither.
-      leftMap(_ => List(s"Cannot parse $string"))
+  def parseInt(name: String)(data: String): FailFast[Int] =
+    Either.catchOnly[NumberFormatException](data.toInt).
+      leftMap(_ => List(s"$name must be an integer"))
 }
