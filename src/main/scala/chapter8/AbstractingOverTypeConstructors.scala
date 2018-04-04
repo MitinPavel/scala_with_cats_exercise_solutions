bd43937df4414757987e146abd49bf724f4ae6a4
Pavlo Mitin <mitin.pavel@gmail.com>
2018-04-04 16:43:08 +0100

    8.2 Abstracting over Monads
    Code has been refactored according to the book solution.

--- src/main/scala/chapter8/AbstractingOverTypeConstructors.scala
+++ src/main/scala/chapter8/AbstractingOverTypeConstructors.scala
@@ -9,7 +9,7 @@ import cats.instances.list._
 import cats.syntax.traverse._
 import cats.syntax.functor._
 
-class UptimeService[F[_]: Functor](client: UptimeClient[F])(implicit applicative: Applicative[F]) {
+class UptimeService[F[_]: Applicative](client: UptimeClient[F]) {
   def getTotalUptime(hostnames: List[String]): F[Int] =
     hostnames.traverse(client.getUptime).map(_.sum)
 }
