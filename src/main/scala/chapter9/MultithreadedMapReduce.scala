b36f8e9c37d273d926cb53e4c9cae5dbf1d2c4bd
Pavlo Mitin <mitin.pavel@gmail.com>
2018-04-06 21:23:04 +0100

    9.3.3 Implementing parallelFoldMap
    "batchCount" method has been fixed.

--- src/main/scala/chapter9/MultithreadedMapReduce.scala
+++ src/main/scala/chapter9/MultithreadedMapReduce.scala
@@ -8,16 +8,19 @@ import cats.syntax.semigroup._
 
 object MultithreadedMapReduce {
   def parallelFoldMap[A, B: Monoid](values: Vector[A])(f: A => B): Future[B] = {
-    val batches = values.grouped(batchCount).toList
+    val batches = values.grouped(batchCount(values.size)).toList
+
     val futureList = batches.map { as =>
       Future(foldMap(as)(f(_)))
     }
+
     Future.sequence(futureList).map { bs =>
       bs.foldLeft(Monoid[B].empty)(_ |+| _)
     }
   }
 
-  private def batchCount = Runtime.getRuntime.availableProcessors
+  private def batchCount[A](totalCount: Int) =
+    (1.0 * totalCount / Runtime.getRuntime.availableProcessors).ceil.toInt
 
   private def foldMap[A, B: Monoid](as: Vector[A])(f: A => B): B =
     SingleThreadedMapReduce.foldMap(as)(f(_))
