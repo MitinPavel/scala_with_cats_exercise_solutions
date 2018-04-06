ccb3bf141a4510c47be803673e7d577497b99170
Pavlo Mitin <mitin.pavel@gmail.com>
2018-04-06 06:49:45 +0100

    9.1 Parallelizing map and fold
    MapReduce was renamed to SingleThreadedMapReduce.

--- src/main/scala/chapter9/MapReduceCaseStudy.scala
+++ src/main/scala/chapter9/MapReduceCaseStudy.scala
@@ -3,12 +3,12 @@ package chapter9
 import cats.instances.int._
 
 object MapReduceCaseStudy extends App {
-  println(MapReduce.foldMap(Vector("1", "2"))(_.toInt))
+  println(SingleThreadedMapReduce.foldMap(Vector("1", "2"))(_.toInt))
 
   import cats.instances.int._
-  println(MapReduce.foldMap(Vector(1, 2, 3))(identity))
+  println(SingleThreadedMapReduce.foldMap(Vector(1, 2, 3))(identity))
 
   import cats.instances.string._
-  println(MapReduce.foldMap(Vector(1, 2, 3))(_.toString + "! "))
-  println(MapReduce.foldMap("Hello world!".toVector)(_.toString.toUpperCase))
+  println(SingleThreadedMapReduce.foldMap(Vector(1, 2, 3))(_.toString + "! "))
+  println(SingleThreadedMapReduce.foldMap("Hello world!".toVector)(_.toString.toUpperCase))
 }
--- src/main/scala/chapter9/SingleThreadedMapReduce.scala
+++ src/main/scala/chapter9/SingleThreadedMapReduce.scala
@@ -3,7 +3,7 @@ package chapter9
 import cats.Monoid
 import cats.syntax.semigroup._
 
-object MapReduce {
+object SingleThreadedMapReduce {
   def foldMap[A, B: Monoid](as: Vector[A])(f: A => B): B =
     as.foldLeft(Monoid[B].empty)(_ |+| f(_))
 }
