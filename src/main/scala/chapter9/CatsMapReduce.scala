95aa7405a738b98ddba78537fa88d6a2b0597ce1
Pavlo Mitin <mitin.pavel@gmail.com>
2018-04-07 10:05:30 +0100

    9.3.3 Implementing parallelFoldMap
    "parallelFoldMap" has been refactored according to the book solution.

--- src/main/scala/chapter9/CatsMapReduce.scala
+++ src/main/scala/chapter9/CatsMapReduce.scala
@@ -1,30 +1,22 @@
 package chapter9
 
-import cats.Foldable
-
-import cats.Traverse
-import cats.instances.future._ // for Applicative
-import cats.instances.list._ // for Traverse
 import cats.Monoid
-import cats.syntax.foldable._
-
+import cats.instances.future._ // for Applicative and Monad
+import cats.instances.vector._ // for Foldable and Traverse
+import cats.syntax.foldable._ // for combineAll and foldMap
+import cats.syntax.traverse._ // for traverse
+import scala.concurrent._
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
 
 object CatsMapReduce {
-  def parallelFoldMap[A, B: Monoid](values: Vector[A])(f: A => B): Future[B] = {
-    val batches: List[List[A]] = values.grouped(batchCount(values.size)).toList.map(_.toList)
-
-    val fut: Future[List[B]] = Traverse[List].traverse(batches) { as: List[A] =>
-      Future { Foldable[List].foldMap(as)(f) }
-    }
-
-    fut.map { bs: List[B] =>
-      bs.combineAll
-    }
+  def parallelFoldMap[A, B: Monoid](values: Vector[A])(func: A => B): Future[B] = {
+    values
+      .grouped(batchCount(values.size))
+      .toVector
+      .traverse(group => Future(group.toVector.foldMap(func)))
+      .map(_.combineAll)
   }
 
   private def batchCount[A](totalCount: Int) =
     (1.0 * totalCount / Runtime.getRuntime.availableProcessors).ceil.toInt
-
 }
