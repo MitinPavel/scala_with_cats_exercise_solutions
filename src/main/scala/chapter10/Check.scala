870432977795e15f4543be962297d329996395c8
Pavlo Mitin <mitin.pavel@gmail.com>
2018-04-09 07:35:38 +0100

    10.3 Basic Combinators
    "and" has been refactored according to the book solution:
    * "implicit Semigroup[E]" parameter was moved from the class constructor to "and" method

--- src/main/scala/chapter10/Check.scala
+++ src/main/scala/chapter10/Check.scala
@@ -1,20 +1,18 @@
 package chapter10
 
 import cats.Semigroup
+import cats.syntax.either._ // for asLeft and asRight
 
-case class Check[E, A](f: A => Either[E, A])(implicit semigroup: Semigroup[E]) {
+case class Check[E, A](f: A => Either[E, A]) {
   def apply(a: A): Either[E, A] = f(a)
 
-  def and(that: Check[E, A]): Check[E, A] =
+  def and(that: Check[E, A])(implicit semigroup: Semigroup[E]): Check[E, A] =
     Check { a: A => {
-      val thisResult = this.apply(a)
-      val thatResult = that.apply(a)
-
-      (thisResult, thatResult) match {
-        case (Right(_), Right(_)) => Right(a)
-        case (Right(_), Left(thatLeft)) => Left(thatLeft)
-        case (Left(thisLeft), Right(_)) => Left(thisLeft)
-        case (Left(thisLeft), Left(thatLeft)) => Left(semigroup.combine(thisLeft, thatLeft))
+      (this(a), that(a)) match {
+        case (Right(_), Right(_)) => a.asRight
+        case (Left(e1), Right(_)) => e1.asLeft
+        case (Right(_), Left(e2)) => e2.asLeft
+        case (Left(e1), Left(e2)) => semigroup.combine(e1, e2).asLeft
       }
     }
     }
