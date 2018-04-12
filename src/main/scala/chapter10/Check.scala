4c6716afd43836f5fa37d5c4fef35a91bfcb62bf
Pavlo Mitin <mitin.pavel@gmail.com>
2018-04-12 07:21:20 +0100

    10.4 Transforming Data
    "apply" and "map" for Check was copied from the book.

--- src/main/scala/chapter10/Check.scala
+++ src/main/scala/chapter10/Check.scala
@@ -1,11 +1,26 @@
 package chapter10
 
+import cats.Semigroup
 import cats.data.Validated
 
 sealed trait Check[E, A, B] {
-  def apply(a: A): Validated[E, B] =
-    ???
+  def apply(in: A)(implicit s: Semigroup[E]): Validated[E, B]
 
-  def map[C](func: B => C): Check[E, A, C] =
-    ???
+  def map[C](f: B => C): Check[E, A, C] =
+    Map[E, A, B, C](this, f)
+}
+
+object Check {
+  def apply[E, A](pred: Predicate[E, A]): Check[E, A, A] =
+    PureCheck(pred)
+}
+
+final case class Map[E, A, B, C](check: Check[E, A, B], func: B => C) extends Check[E, A, C] {
+  def apply(in: A)(implicit s: Semigroup[E]): Validated[E, C] =
+    check(in).map(func)
+}
+
+final case class PureCheck[E, A](pred: Predicate[E, A]) extends Check[E, A, A] {
+  def apply(in: A)(implicit s: Semigroup[E]): Validated[E, A] =
+    pred(in)
 }
--- src/test/scala/chapter10/CheckSpec.scala
+++ src/test/scala/chapter10/CheckSpec.scala
@@ -1,12 +1,31 @@
 package chapter10
 
 import org.scalatest.FlatSpec
+import scala.util.Try
 import cats.syntax.validated._
 
-class CheckSpec extends FlatSpec {
-  val parseInt: Check[List[String], String, Int] = ???
+import cats.instances.list._
 
-  it should "parse string representation of an integer" in {
-    assert(parseInt("123") == 123.valid[List[String]])
+class CheckSpec extends FlatSpec {
+  val isInt = Pure { str: String =>
+    Try(str.toInt).toOption match {
+      case Some(_) => str.valid[List[String]]
+      case None => List("not an integer").invalid[String]
+    }
+  }
+
+  val intStringCheck = Check(isInt)
+  val parseInt = intStringCheck.map(_.toInt)
+
+  "pure" should "wrap Predicate" in {
+    assert(intStringCheck("123") == "123".valid[List[String]])
+    assert(intStringCheck("1.1") == List("not an integer").invalid[String])
+    assert(intStringCheck("blah") == List("not an integer").invalid[String])
+  }
+
+  "map" should "transform wrapped data" in {
+    assert(parseInt("123" ) == 123.valid[List[String]])
+    assert(parseInt("1.1" ) == List("not an integer").invalid[Int])
+    assert(parseInt("blah") == List("not an integer").invalid[Int])
   }
 }
