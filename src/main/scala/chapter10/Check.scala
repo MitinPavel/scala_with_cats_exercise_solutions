95ded81bea9d39c83c3e267f14f194fa6bc3cf12
Pavlo Mitin <mitin.pavel@gmail.com>
2018-04-13 06:52:17 +0100

    10.4 Transforming Data
    "flatMap" for Check was copied from the book.

--- src/main/scala/chapter10/Check.scala
+++ src/main/scala/chapter10/Check.scala
@@ -8,11 +8,19 @@ sealed trait Check[E, A, B] {
 
   def map[C](f: B => C): Check[E, A, C] =
     Map[E, A, B, C](this, f)
+
+  def flatMap[C](f: B => Check[E, A, C]) =
+    FlatMap[E, A, B, C](this, f)
 }
 
 object Check {
   def apply[E, A](pred: Predicate[E, A]): Check[E, A, A] =
     PureCheck(pred)
+}
+
+final case class FlatMap[E, A, B, C](check: Check[E, A, B], func: B => Check[E, A, C]) extends Check[E, A, C] {
+  def apply(a: A)(implicit s: Semigroup[E]): Validated[E, C] =
+    check(a).withEither(_.flatMap(b => func(b)(a).toEither))
 }
 
 final case class Map[E, A, B, C](check: Check[E, A, B], func: B => C) extends Check[E, A, C] {
--- src/test/scala/chapter10/CheckSpec.scala
+++ src/test/scala/chapter10/CheckSpec.scala
@@ -13,19 +13,37 @@ class CheckSpec extends FlatSpec {
       case None => List("not an integer").invalid[String]
     }
   }
-
-  val intStringCheck = Check(isInt)
-  val parseInt = intStringCheck.map(_.toInt)
+  val isOneSymbol = Pure { str: String =>
+    if (str.length == 1) str.valid[List[String]]
+    else List("has invalid length").invalid[String]
+  }
 
   "pure" should "wrap Predicate" in {
+    val intStringCheck = Check(isInt)
+
     assert(intStringCheck("123") == "123".valid[List[String]])
     assert(intStringCheck("1.1") == List("not an integer").invalid[String])
     assert(intStringCheck("blah") == List("not an integer").invalid[String])
   }
 
   "map" should "transform wrapped data" in {
-    assert(parseInt("123" ) == 123.valid[List[String]])
-    assert(parseInt("1.1" ) == List("not an integer").invalid[Int])
+    val intStringCheck = Check(isInt)
+    val parseInt = intStringCheck.map(_.toInt)
+
+    assert(parseInt("123") == 123.valid[List[String]])
+    assert(parseInt("1.1") == List("not an integer").invalid[Int])
     assert(parseInt("blah") == List("not an integer").invalid[Int])
+  }
+
+  "flatMap" should "have meaningful semantic ;)" in {
+    val intStringCheck = Check(isInt)
+    val oneSymbolIntCheck = intStringCheck.flatMap { a: String =>
+      Check(isOneSymbol)
+    }
+
+    assert(oneSymbolIntCheck("1") == "1".valid[List[String]])
+    assert(oneSymbolIntCheck("11") == List("has invalid length").invalid[String])
+    assert(oneSymbolIntCheck("a") == List("not an integer").invalid[String])
+    assert(oneSymbolIntCheck("aa") == List("not an integer").invalid[String])
   }
 }
