6b55552365ccd382e6e13bd15c79ad3b4382a9bd
Pavlo Mitin <mitin.pavel@gmail.com>
2018-04-18 07:20:28 +0100

    11.3.2 Exercise: BoundedSemiLattice Instances
    Type annotations have been added to instance methods.

--- src/main/scala/chapter11/BoundedSemiLattice.scala
+++ src/main/scala/chapter11/BoundedSemiLattice.scala
@@ -11,15 +11,15 @@ trait BoundedSemiLattice[A] extends Monoid[A] {
 object BoundedSemiLatticeInstances {
   implicit def intBoundedSemiLattice: BoundedSemiLattice[Int] =
     new BoundedSemiLattice[Int] {
-      def combine(a: Int, b: Int) = a.max(b)
+      def combine(a: Int, b: Int): Int = a.max(b)
 
-      def empty = 0
+      def empty: Int = 0
     }
 
   implicit def setBoundedSemiLattice[A]: BoundedSemiLattice[Set[A]] =
     new BoundedSemiLattice[Set[A]] {
-      def combine(a: Set[A], b: Set[A]) = a.union(b)
+      def combine(a: Set[A], b: Set[A]): Set[A] = a.union(b)
 
-      def empty = Set.empty[A]
+      def empty: Set[A] = Set.empty[A]
     }
 }
