f8ac0ee8e750dd6f719fc551e0530dfb19b805c6
Pavlo Mitin <mitin.pavel@gmail.com>
2018-04-19 07:13:18 +0100

    11.3.3 Exercise: Generic GCounter

--- src/main/scala/chapter11/GCounter.scala
+++ src/main/scala/chapter11/GCounter.scala
@@ -1,4 +1,26 @@
 package chapter11
 
-class GCounter {
+import cats.Monoid
+
+final case class GCounter[A](counters: Map[String, A]) {
+  def increment(machine: String, amount: A)(implicit monoid: Monoid[A]): GCounter[A] = {
+    val current = counters.getOrElse(machine, monoid.empty)
+    val incremented = monoid.combine(current, amount)
+    GCounter(counters + (machine -> incremented))
+  }
+
+  def merge(that: GCounter[A])(implicit semi_lattice: BoundedSemiLattice[A]): GCounter[A] = {
+    val machines = counters.keys ++ that.counters.keys
+    val merged = machines.foldLeft(Map[String, A]()) { (acc, machine) =>
+      val thisCount = counters.getOrElse(machine, semi_lattice.empty)
+      val thatCount = that.counters.getOrElse(machine, semi_lattice.empty)
+      val count = semi_lattice.combine(thisCount, thatCount)
+
+      acc + (machine -> count)
+    }
+    GCounter(merged)
+  }
+
+  def total(implicit monoid: Monoid[A]): A =
+    Monoid[A].combineAll(counters.values)
 }
---
+++ src/test/scala/chapter11/GCounterSpec.scala
@@ -0,0 +1,48 @@
+package chapter11
+
+import org.scalacheck.{Arbitrary, Gen}
+import org.scalatest.FlatSpec
+import org.scalatest.prop.Checkers
+import cats.Monoid
+
+class GCounterSpec extends FlatSpec with Checkers {
+  implicit def arbitrary(implicit a: Arbitrary[List[Int]]) =
+    Arbitrary(Gen.containerOf[List, Int](Gen.chooseNum(1, 10)))
+
+  "total" should "sum up collected counts" in {
+    import cats.instances.int._
+
+    check((counts: List[Int]) => {
+      val counter = increment("a", GCounter[Int](Map()), counts)
+
+      counts.sum == counter.total
+    })
+  }
+
+  "merge" should "merge counts" in {
+    import cats.instances.int._
+
+    check((input1: List[List[Int]], input2: List[List[Int]]) => {
+      var counter1 = GCounter[Int](Map())
+      var counter2 = GCounter[Int](Map())
+
+      input1.zipAll(input2, List(), List()).foreach { (ls) =>
+        counter1 = increment("one", counter1, ls._1)
+        counter2 = increment("two", counter2, ls._2)
+
+        counter1 = counter1.merge(counter2)(BoundedSemiLatticeInstances.intBoundedSemiLattice)
+        counter2 = counter2.merge(counter1)(BoundedSemiLatticeInstances.intBoundedSemiLattice)
+      }
+
+      val expected = (input1.flatten ++ input2.flatten).sum
+      val actual1 = counter1.total
+      val actual2 = counter2.total
+
+      expected == actual1 && actual1 == actual2
+    })
+  }
+
+  private def increment[A](machine: String, counter: GCounter[A], counts: List[A])(implicit m: Monoid[A]) =
+    counts.
+      foldLeft(counter) { (a, i) => a.increment(machine, i) }
+}
